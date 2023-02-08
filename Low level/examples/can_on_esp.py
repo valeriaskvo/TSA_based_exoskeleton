3529c9bd4bd9e1a59f4db3810f2d33a14d626f34
Slauva <v.koshman@innopolis.university>
2023-02-08 14:11:26 +0300

    feat(can): add config files for the board and CAN bus interface

--- Low level/examples/can_on_esp.py
+++ Low level/examples/can_on_esp.py
@@ -6,9 +6,7 @@ import struct
 CAN_TX = 26
 CAN_RX = 27
 
-can = CAN(0, tx=CAN_TX, rx=CAN_RX,
-          extframe=False, mode=CAN.NORMAL,
-          baudrate=1000000)
+can = CAN(0, tx=CAN_TX, rx=CAN_RX, extframe=False, mode=CAN.NORMAL, baudrate=1000000)
 
 i = 0
 while True:
@@ -22,5 +20,5 @@ while True:
         can.send(tuple(command), 0x1)   # send a message
         i = 0
     can.clear_rx_queue()        # clear messages in the FIFO
-    can.clear_tx_queue()  
+    can.clear_tx_queue()
     sleep(0.01)
