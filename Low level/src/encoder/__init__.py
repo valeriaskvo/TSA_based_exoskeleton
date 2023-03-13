c313db05fe60b21bc85e1ed32ff049719c21aef1
Lumiwarum <78841564+Lumiwarum@users.noreply.github.com>
2023-03-13 08:20:54 -0700

    Encoder v0.1

---
+++ Low level/src/encoder/__init__.py
@@ -0,0 +1,47 @@
+import machine
+import utime as time
+from machine import Pin 
+
+class Encoder:
+
+    def __init__(self, PinA, PinB):  # method is automatically called when new instance of class is created
+
+        self.ChA = Pin(PinA, Pin.IN, None)  # encoder channel A, PinA, no internal pull-up or pull-down resistor
+        self.ChB = Pin(PinB, Pin.IN, None)  # encoder channel A, PinB, no internal pull-up or pull-down resistor
+        self.enable_ChA_irq(self.update)
+        self.enable_ChB_irq(self.update)
+        self.state = (self.ChA.value() << 1) + self.ChB.value()
+        self.position = 0
+
+    def enable_ChA_irq(self, callback=None):
+        self.ChA.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=callback)
+
+    def enable_ChB_irq(self, callback=None):
+        self.ChB.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=callback)
+
+    def update(self, pin):
+        new_state = (self.ChA.value() << 1) | self.ChB.value()
+        if new_state == self.state:
+            return
+
+        transition = (self.state << 2) | new_state
+
+        if transition == 0b1110:
+            self.position += 1
+        if transition == 0b1011:
+            self.position -= 1
+        self.state = new_state
+
+    def get_pos(self):
+        return self.position
+        
+     
+if __name__ == "__main__":
+  machine.freq(240000000)
+
+  enc = Encoder(34, 35)
+  count = 0
+
+  while True:
+    print(enc.get_pos())
+    sleep(0.1)
