5b52e3d88bc133f8dfda3ecb0395b72230dcf1a6
Lumiwarum <78841564+Lumiwarum@users.noreply.github.com>
2023-03-13 18:36:35 +0300

    Added comments to the encoder class

--- Low level/src/encoder/__init__.py
+++ Low level/src/encoder/__init__.py
@@ -3,16 +3,22 @@ import utime as time
 from machine import Pin 
 
 class Encoder:
+    """
+    Encoder class that tracks an encoder using interruptions 
+    PinA and PinB are integer numbers of the pins that are connected to the A and B channels from encoder
+    
+    Currently there is only one availvable method, that returns number of ticks from the initial position
+    """
 
-    def __init__(self, PinA, PinB):  # method is automatically called when new instance of class is created
+    def __init__(self, PinA, PinB):  
 
         self.ChA = Pin(PinA, Pin.IN, None)  # encoder channel A, PinA, no internal pull-up or pull-down resistor
         self.ChB = Pin(PinB, Pin.IN, None)  # encoder channel A, PinB, no internal pull-up or pull-down resistor
         self.enable_ChA_irq(self.update)
         self.enable_ChB_irq(self.update)
         self.state = (self.ChA.value() << 1) + self.ChB.value()
-        self.position = 0
-
+        self.position = 0 
+        
     def enable_ChA_irq(self, callback=None):
         self.ChA.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=callback)
 
